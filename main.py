7f66af831fa94a9b1961706169fe8aecbcf415d7
linogaliana <lino.galiana@insee.fr>
2024-01-24 14:39:34 +0000

    delete csv

--- main.py
+++ main.py
@@ -22,9 +22,10 @@ args = parser.parse_args()
 
 # Paramètres YAML
 config = imp.import_yaml_config("configuration/config.yaml")
+base_url = "https://minio.lab.sspcloud.fr/projet-formation/ensae-reproductibilite/data/raw/"
 API_TOKEN = config.get("jeton_api")
-LOCATION_TRAIN = config.get("train_path", "train.csv")
-LOCATION_TEST = config.get("test_path", "test.csv")
+LOCATION_TRAIN = config.get("train_path", f"{base_url}/train.csv")
+LOCATION_TEST = config.get("test_path", f"{base_url}/test.csv")
 TEST_FRACTION = config.get("test_fraction", .1)
 N_TREES = args.n_trees
 
