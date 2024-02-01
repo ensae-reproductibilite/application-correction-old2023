a950f57dd0c8fa73f4780eba18115b0ad85fca6e
linogaliana <lino.galiana@insee.fr>
2024-02-01 10:32:51 +0000

    corrige erreur

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ args = parser.parse_args()
 
 # Paramètres YAML
 config = imp.import_yaml_config("configuration/config.yaml")
-base_url = "https://minio.lab.sspcloud.fr/projet-formation/ensae-reproductibilite/data/raw/"
+base_url = "https://minio.lab.sspcloud.fr/projet-formation/ensae-reproductibilite/data/raw"
 API_TOKEN = config.get("jeton_api")
 LOCATION_TRAIN = config.get("train_path", f"{base_url}/train.csv")
 LOCATION_TEST = config.get("test_path", f"{base_url}/test.csv")
